9da18690866c8b762c52fb3b9d22830c7cfd314e
Cristyn Howard <cristyn.howard@mail.utoronto.ca>
2017-12-17 01:29:36 -0500

    Set up constants/variables. Created setup/reset functions.

--- stab-bot-5000.ino
+++ stab-bot-5000.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+Servo myServo;
+
+const int servoPin = 9;
+const int sensorPin = A0;
+const int baseAngle = 1020;
+const int baseLight;
+
+int lightLevel;
+
+void setup() {
+  myServo.attach(servoPin);
+  reset();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+
+}
+
+void reset() {
+  baseLight = analogRead(sensorPin);
+  myServo.write(baseAngle);
 }
